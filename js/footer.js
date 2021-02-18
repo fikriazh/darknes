375c1452591b0599d4df878978a0ed1814c9a410
fikriazh <fikriazh@users.noreply.github.com>
2021-02-18 07:13:00 +0000

    deploy: 5d113daa400f8c7ce9af43638b862f7be0da5453

---
+++ js/footer.js
@@ -0,0 +1,28 @@
+"use strict";
+let blinkTextMenuLinks = document.querySelectorAll(".footer_animate");
+blinkTextMenuLinks.forEach(link => {
+    let letters = link.textContent.split("");
+    link.textContent = "";
+    letters.forEach((letter, i) => {
+        i += 1;
+        let span = document.createElement("span");
+        let delay = i / 20;
+        if (i % 2 === 0) {
+            delay -= 0.1;
+        }
+        else {
+            delay += 0.05;
+        }
+        let letterOut = document.createElement("span");
+        letterOut.textContent = letter;
+        letterOut.style.transitionDelay = `${delay}s`;
+        letterOut.classList.add("out");
+        span.append(letterOut);
+        let letterIn = document.createElement("span");
+        letterIn.textContent = letter;
+        letterIn.style.transitionDelay = `${delay}s`;
+        letterIn.classList.add("in");
+        span.append(letterIn);
+        link.append(span);
+    });
+});
