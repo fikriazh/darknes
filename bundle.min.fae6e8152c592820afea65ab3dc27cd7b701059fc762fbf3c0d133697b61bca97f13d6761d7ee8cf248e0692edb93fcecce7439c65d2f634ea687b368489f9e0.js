611e44d68b03db7160b415efe0ac378dd36efab9
fikriazh <fikriazh@users.noreply.github.com>
2021-02-19 13:17:11 +0000

    deploy: d8171b4024850bd9f3311295b1cc60420c52eabf

--- bundle.min.fae6e8152c592820afea65ab3dc27cd7b701059fc762fbf3c0d133697b61bca97f13d6761d7ee8cf248e0692edb93fcecce7439c65d2f634ea687b368489f9e0.js
+++ bundle.min.fae6e8152c592820afea65ab3dc27cd7b701059fc762fbf3c0d133697b61bca97f13d6761d7ee8cf248e0692edb93fcecce7439c65d2f634ea687b368489f9e0.js
@@ -1,10 +1,4 @@
-const summaryInclude=60;let fuseOptions={shouldSort:true,includeMatches:true,tokenize:true,matchAllTokens:true,threshold:0.0,location:0,distance:100,maxPatternLength:64,minMatchCharLength:3,keys:[{name:"title",weight:0.8},{name:"categories",weight:0.5},{name:"contents",weight:0.4}]};function getUrlParameter(name){name=name.replace(/[\[]/,'\\[').replace(/[\]]/,'\\]');let regex=new RegExp('[\\?&]'+name+'=([^&#]*)');let results=regex.exec(location.search);return results===null?'':decodeURIComponent(results[1].replace(/\+/g,' '));}
-let searchQuery=getUrlParameter('q');if(searchQuery){document.getElementById("search-query").value=searchQuery;executeSearch(searchQuery);}
-else{document.getElementById('search-results').innerHTML="<p class=\"no-results\">Search Title, Content etc.</p>";}
-function executeSearch(searchQuery){fetch("index.json").then(function(response){return response.json()}).then(function(data){let fuse=new Fuse(data,fuseOptions);let result=fuse.search(searchQuery);if(result.length>0){populateResults(result);}else{document.getElementById('search-results').innerHTML="<p class=\"no-results\">No matches found</p>";}});}
-function populateResults(result){result.forEach(function(value,key){let contents=value.item.contents;let snippet="";let snippetHighlights=[];snippetHighlights.push(searchQuery);if(snippet.length<1){snippet+=contents.substring(0,summaryInclude*1.75);}
-snippet+="…";var elem=document.createElement('textarea');elem.innerHTML=snippet;var decoded=elem.value;let frag=document.getElementById('search-result-template').content.cloneNode(true);frag.querySelector(".search_summary").setAttribute("id","summary-"+key);frag.querySelector(".search_link").setAttribute("href",value.item.permalink);frag.querySelector(".search_title").textContent=value.item.title;frag.querySelector(".search_snippet").textContent=decoded;let categories=value.item.categories;if(categories){frag.querySelector(".search_categories").textContent=categories;}else{frag.querySelector(".search_ifcategories").remove();}
-snippetHighlights.forEach(function(snipvalue,snipkey){let markjs=new Mark(frag);markjs.mark(snipvalue);});document.getElementById("search-results").appendChild(frag);});};/*!
+/*!
 * Fuse.js v3.2.1 - Lightweight fuzzy-search (http://fusejs.io)
 *
 * Copyright (c) 2012-2017 Kirollos Risk (http://kiro.me)
